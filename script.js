07e050d42deaeb7650a8166506d5af75a4347c5c
sahal akma <69509304+sahalakma@users.noreply.github.com>
2022-06-20 21:43:22 +0530

    Removed Unnecessary Items from bar Chart

--- script.js
+++ script.js
@@ -12,10 +12,13 @@ const data =  {
     labels:labels,
     datasets: [{
         label: "",
-        backgroundColor : "hsl(10, 79%, 65%)",
+        backgroundColor: "hsl(10, 79%, 65%)",
+        hoverBackgroundColor: "hsl(186, 34%, 60%)",
+        borderRadius: "5" ,
         data: [20,35,52,31,25,40,30] 
     }]
 };
+
 
 //tooltip
 const titleTooltip = (tooltipItems) => {
@@ -31,11 +34,27 @@ options: {
             yAlign: "bottom",
             bodyColor : "#fff",
             callbacks: {
-                title: titleTooltip,
-
-            }    
+                title: titleTooltip
+            },
+            displayColors: false    
             
         }
+    },
+    scales: {
+        x: {
+            grid: {
+                display:false
+            }
+        },
+        y: {
+            grid:{
+                display:false
+            },
+            ticks:{
+                display:false
+            }
+        }
+
     }
 }
 };
