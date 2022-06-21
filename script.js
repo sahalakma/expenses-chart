5f9996d639af0dd0bc382bd1e046ed45e42fba13
sahal akma <69509304+sahalakma@users.noreply.github.com>
2022-06-21 20:54:27 +0530

    JSON Data Added

--- script.js
+++ script.js
@@ -1,66 +1,91 @@
-const labels = [
-    "mon",
-    "tue",
-    "wed",
-    "thu",
-    "fri",
-    "sat",
-    "sun"
-];
+// ajax Block 
 
-const data =  {
-    labels:labels,
-    datasets: [{
-        label: "",
-        backgroundColor: "hsl(10, 79%, 65%)",
-        hoverBackgroundColor: "hsl(186, 34%, 60%)",
-        borderRadius: "5" ,
-        data: [20,35,52,31,25,40,30] 
-    }]
-};
+const xmlhttp = new XMLHttpRequest();
+
+xmlhttp.open('GET', "data.json");
+xmlhttp.send();
+
+xmlhttp.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status == 200){
+    const datapoints = JSON.parse(this.responseText);
+    console.log(datapoints);
+
+    const labelsweek = datapoints.map(function(index) {
+        return index.day;
+    });
+    const labelsAmount = datapoints.map(function(index) {
+        return index.amount;
+    });
 
 
-//tooltip
-const titleTooltip = (tooltipItems) => {
-    return  ;
-}
+    const data =  {
+        labels: labelsweek,
+        datasets: [{
+            label: "",
+            backgroundColor: "hsl(10, 79%, 65%)",
+            hoverBackgroundColor: "hsl(186, 34%, 60%)",
+            borderRadius: "5" ,
+            data: labelsAmount 
+        }]
+    };
 
-const config = {
-type : "bar",
-data : data,
-options: {
-    plugins: {
-        tooltip: {
-            yAlign: "bottom",
-            bodyColor : "#fff",
-            callbacks: {
-                title: titleTooltip
-            },
-            displayColors: false    
-            
-        }
-    },
-    scales: {
-        x: {
-            grid: {
-                display:false
+    const titleTooltip = (tooltipItems) => {
+        return  ;
+    }
+    
+    const config = {
+    type : "bar",
+    data : data,
+    options: {
+        plugins: {
+            tooltip: {
+                yAlign: "bottom",
+                bodyColor : "#fff",
+                callbacks: {
+                    title: titleTooltip
+                },
+                displayColors: false    
+                
             }
         },
-        y: {
-            grid:{
-                display:false
+        scales: {
+            x: {
+                grid: {
+                    display:false
+                }
             },
-            ticks:{
-                display:false
+            y: {
+                grid:{
+                    display:false
+                },
+                ticks:{
+                    display:false
+                }
             }
+    
         }
+    }
+    };
+
+    const myChart = new Chart (
+        document.getElementById("myChart"),
+        config
+        );
 
     }
-}
-};
+}   
+
+// const labels = [
+//     "mon",
+//     "tue",
+//     "wed",
+//     "thu",
+//     "fri",
+//     "sat",
+//     "sun"
+// ];
+
+//tooltip
 
 
-const myChart = new Chart (
-    document.getElementById("myChart"),
-    config
-    );
+
